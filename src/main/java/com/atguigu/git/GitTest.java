12801b43b597957fb7ff12b891cccd291d3e4f06
Jane <Jane310402@outlook.com>
2022-11-20 10:11:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * @author Jane
+ * @create 2022-11-20 9:30
+ */
+public class GitTest {
+    public static void main(String[] args){
+        System.out.println("hello git!");
+    }
+}
